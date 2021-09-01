a44cb5d17783693e5a7d6d1c17898e04ac2896b7
Saurabh Kamble <saurabhkamble1989@gmail.com>
2021-09-01 18:18:03 +0530

    added demo-app

--- hello-lit.ts
+++ hello-lit.ts
@@ -1,3 +1,9 @@
 import { HelloLit } from './src/HelloLit.js';
 
 window.customElements.define('hello-lit', HelloLit);
+
+declare global {
+    interface HTMLElementTagNameMap {
+        "hello-lit": HelloLit
+    }
+}
